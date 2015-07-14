9fbf9b4597cf60250758d12c77a8efc788e5432a
Joaquim Alvino de Mesquita Neto <joaquimmnetto@gmail.com>
2015-07-14 14:46:27 -0300

    Adicionado args de inicialização

--- src/br/alimec/server/main/Main.java
+++ src/br/alimec/server/main/Main.java
@@ -1,40 +1,53 @@
 package br.alimec.server.main;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
+
+import com.sun.jmx.remote.internal.ArrayQueue;
 
 import br.alimec.server.connect.Server;
 import br.alimec.server.connect.ServerWorker;
 
 public class Main {
 
-	//TODO: STATUS ATUAL: TESTAR O DAO DAS PLANILHAS, CLIENTE-SERVIDOR ESTA OKAY :D
+	// TODO: STATUS ATUAL: TESTAR O DAO DAS PLANILHAS, CLIENTE-SERVIDOR ESTA
+	// OKAY :D
+
+	private static int porta = 9009;
+	private static int threadPoolSize = 5;
 
 	public static void main(String[] args) {
-		int porta = 9009;
-		Executor exec = Executors.newFixedThreadPool(5);
-		
+
+		parseArgs(args);
+
+		Executor exec = Executors.newFixedThreadPool(threadPoolSize);
+
 		Log log = new Log(System.out, System.err);
-		
+
 		log.println("Iniciando Servidor na porta " + porta);
 		Server server = new Server(porta);
-		boolean running = true;	
-		while(running){
+		boolean running = true;
+		while (running) {
 			try {
 				log.println("\nAguardando conexoes...");
 				ServerWorker worker = server.listen();
-				log.println("Conexao com " + worker.getAddress()+":"+worker.getPort() + " estabelecida.");
-				
+				log.println("Conexao com " + worker.getAddress() + ":"
+						+ worker.getPort() + " estabelecida.");
+
 				exec.execute(worker);
-				
+
 			} catch (Exception e) {
 				e.printStackTrace();
-	//			log.printException(e, server.getLastCommand());
-			
-			} 
+				// log.printException(e, server.getLastCommand());
+
+			}
 		}
-		
+
 		try {
 			server.close();
 		} catch (IOException e) {
@@ -43,9 +56,30 @@ public class Main {
 		}
 
 	}
-//
-//	public static Log getLog() {
-//		return log;
-//	}
 
+	//
+	// public static Log getLog() {
+	// return log;
+	// }
+
+	private static void parseArgs(String[] args) {
+		ArrayDeque<String> argsQueue = new ArrayDeque<String>(
+				Arrays.asList(args));
+
+		while (!argsQueue.isEmpty()) {
+			if(argsQueue.peek().equals("-u")){
+				//TODO: atualizar os nomes das planilhas
+			}
+			//listen
+			if (argsQueue.peek().equals("-l")) {
+				argsQueue.removeFirst();
+				porta = Integer.parseInt(argsQueue.removeFirst());
+			}
+			//thread pool
+			if (argsQueue.peek().equals("-tp")) {
+				argsQueue.removeFirst();
+				threadPoolSize = Integer.parseInt(argsQueue.removeFirst());
+			}
+		}
+	}
 }
